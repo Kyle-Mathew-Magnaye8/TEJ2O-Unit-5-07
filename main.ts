75aece7d41843e4ea73b439f97abe62f64ef3908
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-05-23 22:26:56 -0400

    added comment

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Kyle Matthew
+ * Created on: Apr 2026
+ * This program has 2 - 28BYJ-48 DC 5V stepper motor and a distance sensor, 
+ * if there is an object within 10 cm of the distance sensor, the motors stop, 
+ * the card reverses 10 cm, it then turns 90° and then continues moving forward.
 */
 
 basic.showString('Hello, World!')
